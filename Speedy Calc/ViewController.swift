9d75f03a7056e6a87a038c6f4c04906d74bc009f
Twinzs <timothy.winz@shp.org>
2022-06-06 18:04:34 -0400

    Initial Commit

---
+++ Speedy Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speedy Calc
+//
+//  Created by Timmy WInz on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
